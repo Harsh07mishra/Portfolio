e4ef936aa0b5faf14f6cc485c1642f9f83f7278c
Harsh Mishra <harsh2204mishra@gmail.com>
2025-02-12 11:38:01 +0530

    Up until projects and services

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import { useTheme } from '../ThemeContext';
-import { fadeIn, staggerContainer } from '../utils/motion';
-import { logo, light, dark } from '../assets';
-import { navLinks } from '../constants';
-import style from './styles/navbar.module.css';
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { useTheme } from "../ThemeContext";
+import { fadeIn, staggerContainer } from "../utils/motion";
+import { logo, light, dark } from "../assets";
+import { navLinks } from "../constants";
+import style from "./styles/navbar.module.css";
 
 const Navbar = () => {
   const { theme, toggleTheme } = useTheme();
-  const [active, setActive] = useState('');
+  const [active, setActive] = useState("");
   const [scrolled, setScrolled] = useState(false);
   const [isOpen, setOpen] = useState(false);
 
@@ -26,22 +26,26 @@ const Navbar = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
     <div className={style.navbar_container}>
-      <nav className={`${style.navbar} ${scrolled ? 'fixed' : ''}`}>
+      <nav className={`${style.navbar} ${scrolled ? "fixed" : ""}`}>
         <motion.button
           type="button"
           onClick={() => {
             window.scrollTo(0, 0);
           }}
-          whileHover={{ scale: 1.12, backgroundColor: 'var(--dark-blue)', transition: { type: 'spring', damping: 20, stiffness: 300 } }}
+          whileHover={{
+            scale: 1.12,
+            backgroundColor: "var(--dark-blue)",
+            transition: { type: "spring", damping: 20, stiffness: 300 },
+          }}
           className={style.logo_btn}
         >
           <img src={logo} alt="My logo" className={style.logo} loading="lazy" />
@@ -69,10 +73,20 @@ const Navbar = () => {
               className={style.theme_btn}
               onClick={toggleTheme}
             >
-              {theme === 'light' ? (
-                <img src={light} className={style.theme_img} alt="light theme" loading="lazy" />
+              {theme === "light" ? (
+                <img
+                  src={light}
+                  className={style.theme_img}
+                  alt="light theme"
+                  loading="lazy"
+                />
               ) : (
-                <img src={dark} className={style.theme_img} alt="dark theme" loading="lazy" />
+                <img
+                  src={dark}
+                  className={style.theme_img}
+                  alt="dark theme"
+                  loading="lazy"
+                />
               )}
             </button>
           </li>
@@ -83,16 +97,16 @@ const Navbar = () => {
           className={style.hamburger}
           onClick={handleMenuClick}
           onKeyDown={(e) => {
-            if (e.key === 'Enter') {
+            if (e.key === "Enter") {
               handleMenuClick();
             }
           }}
           role="button"
           tabIndex={0}
         >
-          <span className={`${style.line} ${isOpen ? style.open : ''}`} />
-          <span className={`${style.line} ${isOpen ? style.open : ''}`} />
-          <span className={`${style.line} ${isOpen ? style.open : ''}`} />
+          <span className={`${style.line} ${isOpen ? style.open : ""}`} />
+          <span className={`${style.line} ${isOpen ? style.open : ""}`} />
+          <span className={`${style.line} ${isOpen ? style.open : ""}`} />
         </div>
 
         {/* mobile menu */}
@@ -101,7 +115,9 @@ const Navbar = () => {
           initial="hidden"
           whileInView="show"
           viewport={{ once: true, amount: 0.25 }}
-          className={`${style.mobilemenu} ${isOpen ? 'h-screen opacity-100' : 'h-0'}`}
+          className={`${style.mobilemenu} ${
+            isOpen ? "h-screen opacity-100" : "h-0"
+          }`}
         >
           <li>
             <button
@@ -109,10 +125,20 @@ const Navbar = () => {
               className={`${style.theme_btn} mt-[130px]`}
               onClick={toggleTheme}
             >
-              {theme === 'light' ? (
-                <img src={light} className={style.theme_img} alt="light theme" loading="lazy" />
+              {theme === "light" ? (
+                <img
+                  src={light}
+                  className={style.theme_img}
+                  alt="light theme"
+                  loading="lazy"
+                />
               ) : (
-                <img src={dark} className={style.theme_img} alt="dark theme" loading="lazy" />
+                <img
+                  src={dark}
+                  className={style.theme_img}
+                  alt="dark theme"
+                  loading="lazy"
+                />
               )}
             </button>
           </li>
@@ -124,7 +150,7 @@ const Navbar = () => {
               <motion.a
                 href={link.url}
                 className={style.mobile_font}
-                variants={fadeIn('right', '', index * 0.2, 1)}
+                variants={fadeIn("right", "", index * 0.2, 1)}
                 initial="hidden"
                 whileInView="show"
                 onClick={() => {
